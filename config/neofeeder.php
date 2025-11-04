d5b994a3b072d5c8720857132a74a03b1c73bf60
Reza Rafiq MZ <rezarafiqmz@gmail.com>
2025-11-04 20:20:06 +0800

    fix(config): ensure neofeeder.php returns array and saved as UTF-8 without BOM

---
+++ config/neofeeder.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'url' => env('URL_NEOFEEDER', ''),
+    'username' => env('USR_NEOFEEDER', ''),
+    'password' => env('PAS_NEOFEEDER', ''),
+    'timeout' => env('NEOFEEDER_TIMEOUT', 15),
+];
